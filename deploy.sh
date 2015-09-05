52ddb9fcd8cadb88aa292208d327680652b47007
Stefan Eng <stefaneng13@gmail.com>
2015-09-05 13:03:46 -0700

    Change cp directory to inside of public_html

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,10 @@ tar -xzf package.tgz
 rm package.tgz
 
 # Make directory if it does not exist
-mkdir -p ${HOME}/webdrive/primer_project
+mkdir -p ${HOME}/webdrive/public_html/primer_project
 
 # Copy public_html folder from project to webdrive
-cp -rf public_html/* ${HOME}/webdrive/primer_project
+cp -rf public_html/* ${HOME}/webdrive/public_html/primer_project
+
+# Cleanup folder
+rm -rf public_html
