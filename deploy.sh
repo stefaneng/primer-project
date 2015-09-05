1d824a28b282b157d9315909093dc3a1634bcb1d
Stefan Eng <stefaneng13@gmail.com>
2015-09-05 12:59:53 -0700

    Better deploy script
    Deploys to /primer_project instead of root directory

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 CURR_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)
-cd CURR_DIR
+cd $CURR_DIR
 
 # Extract the package
 tar -xzf package.tgz
 rm package.tgz
 
+# Make directory if it does not exist
+mkdir -p ${HOME}/webdrive/primer_project
+
 # Copy public_html folder from project to webdrive
-cp -rf public_html ${HOME}/webdrive
+cp -rf public_html/* ${HOME}/webdrive/primer_project
